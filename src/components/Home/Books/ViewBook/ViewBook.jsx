6f45b92ed2b8919545ef7cb2cbb7f7f3968e66fe
DevIdol <devidol.mm@gmail.com>
2022-08-02 19:41:38 +0630

    Modified on 2 August 2022

--- src/components/Home/Books/ViewBook/ViewBook.jsx
+++ src/components/Home/Books/ViewBook/ViewBook.jsx
@@ -9,46 +9,52 @@ const ViewBook = () => {
       quantity > 1 && setQuantity((pre) => pre - 1);
     }
     if (type === "inc") {
-     quantity < 5 && setQuantity((pre) => pre + 1);
+      quantity < 4 && setQuantity((pre) => pre + 1);
     }
   };
   return (
     <div className={styles.books}>
-      <img className={styles.bookImg} src={img} alt="book_img" />
       <div className={styles.bookDesc}>
-        <p className={styles.bookName}>ဂရုမစိုက်ခြင်း အနုပညာ</p>
-        <p className={styles.author}>ဆရာ ချမ်းမြေ့ဝင်း ဘာသာပြန်သည်</p>
-        <p className={styles.pages}>468 pages</p>
+        <img className={styles.bookImg} src={img} alt="book_img" />
         <div>
-          <p>
-            <span className={styles.amount}>Quantity:</span>
-            <button
-              style={{
-                backgroundColor: quantity <= 1 && "transparent",
-                color: (quantity <= 1 || quantity) && "grey",
-              }}
-              className={styles.decrease}
-              onClick={() => handleQuantity("dec")}
-            >
-              -
-            </button>
-            <span className={styles.quantity}>{quantity}</span>
-            <button
-              style={{
-                backgroundColor: quantity >= 5 && "transparent",
-                color: (quantity >= 5 || quantity) && "grey",
-              }}
-              className={styles.increase}
-              onClick={() => handleQuantity("inc")}
-            >
-              +
-            </button>
-          </p>
+          <p className={styles.bookName}>ဂရုမစိုက်ခြင်း အနုပညာ</p>
+          <p className={styles.author}>ဆရာ ချမ်းမြေ့ဝင်း ဘာသာပြန်သည်</p>
+          <p className={styles.pages}>468 pages</p>
+          <div>
+            <p>
+              <span className={styles.amount}>Quantity:</span>
+              <button
+                style={{
+                  backgroundColor: quantity <= 1 && "transparent",
+                  color: quantity <= 1 && "#ccc",
+                }}
+                className={styles.decrease}
+                onClick={() => handleQuantity("dec")}
+              >
+                -
+              </button>
+              <span className={styles.quantity}>{quantity}</span>
+              <button
+                style={{
+                  backgroundColor: quantity >= 4 && "transparent",
+                  color: quantity >= 4 && "#ccc",
+                }}
+                className={styles.increase}
+                onClick={() => handleQuantity("inc")}
+              >
+                +
+              </button>
+            </p>
+          </div>
+          <div className={styles.shopBtnMax}>
+            <button className={styles.shopNow}>Shop Now</button>
+            <button className={styles.addCart}>Add To Cart</button>
+          </div>
         </div>
-        <div className={styles.shopBtn}>
-          <button className={styles.shopNow}>Shop Now</button>
-          <button className={styles.addCart}>Add To Cart</button>
-        </div>
+      </div>
+      <div className={styles.shopBtnMin}>
+        <button className={styles.shopNow}>Shop Now</button>
+        <button className={styles.addCart}>Add To Cart</button>
       </div>
     </div>
   );
