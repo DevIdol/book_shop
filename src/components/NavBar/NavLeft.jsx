b2c13a30ba2c806ef540a79fbb4f7dd1c1288298
DevIdol <devidol.mm@gmail.com>
2022-08-05 14:24:53 +0630

    Modified on 5 August 2022

--- src/components/NavBar/NavLeft.jsx
+++ src/components/NavBar/NavLeft.jsx
@@ -1,7 +1,7 @@
 import styles from "./NavLeft.module.css";
 
 const NavLeft = () => {
-  return <h1 className={styles.logo}>Logo</h1>;
+  return <h1 className={styles.logo}>OBShop</h1>;
 };
 
 export default NavLeft;
