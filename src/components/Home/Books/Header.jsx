51bdf9bb9d1dde6aefdf080f2aec663b235bf1de
DevIdol <devidol.mm@gmail.com>
2022-08-06 11:36:22 +0630

    Modified on 6 August 2022

--- src/components/Home/Books/Header.jsx
+++ src/components/Home/Books/Header.jsx
@@ -4,31 +4,29 @@ import styles from "./Header.module.css";
 import Tooltip from "@mui/material/Tooltip";
 import { Fragment } from "react";
 
-
 const Header = ({ title, showList, toggleShowList }) => {
   return (
     <Fragment>
-     
       <div className={styles.header}>
-      <h1 className={styles.headerTitle}>{title}</h1>
-      <div className={styles.headerRight}>
-        <span className={styles.listIcon} onClick={toggleShowList}>
-          {showList ? (
-            <Tooltip title="View Grid" arrow>
-              <span>
-                <BsFillGrid3X3GapFill size={16} />
-              </span>
-            </Tooltip>
-          ) : (
-            <Tooltip title="View List" arrow>
-              <span>
-                <FaList size={16} />
-              </span>
-            </Tooltip>
-          )}
-        </span>
+        <h1 className={styles.headerTitle}>{title}</h1>
+        <div className={styles.headerRight}>
+          <span className={styles.listIcon} onClick={toggleShowList}>
+            {showList ? (
+              <Tooltip title="View Grid" arrow>
+                <span>
+                  <BsFillGrid3X3GapFill size={16} />
+                </span>
+              </Tooltip>
+            ) : (
+              <Tooltip title="View List" arrow>
+                <span>
+                  <FaList size={16} />
+                </span>
+              </Tooltip>
+            )}
+          </span>
+        </div>
       </div>
-    </div>
     </Fragment>
   );
 };
--- src/components/NavBar/NavLeft.jsx
+++ src/components/NavBar/NavLeft.jsx
@@ -1,7 +1,15 @@
+import { Tooltip } from "@mui/material";
+import { Link } from "react-router-dom";
 import styles from "./NavLeft.module.css";
 
 const NavLeft = () => {
-  return <h1 className={styles.logo}>OBShop</h1>;
+  return (
+    <Link to="/book_shop">
+      <Tooltip title="Online Book Shop" arrow>
+        <h1 className={styles.logo}>𝓞𝓑𝓢</h1>
+      </Tooltip>
+    </Link>
+  );
 };
 
 export default NavLeft;
