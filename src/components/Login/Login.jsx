1d2a326a91b58f53b8f2ac5b18b79de152edfbc8
DevIdol <devidol.mm@gmail.com>
2022-08-09 22:08:13 +0630

    Modified on 9 August 2022

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,35 @@
+import { Link } from "react-router-dom";
 import styles from "./Login.module.css";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import { useState } from "react";
 
 const Login = () => {
+  const [showPass, setShowPass] = useState(false);
+  const showPassword = () => setShowPass(!showPass);
   return (
     <div className={styles.loginPage}>
-      <h4>Welcome Back</h4>
+      <h5>Welcome Back, Please Login</h5>
       <form className={styles.form}>
-        <input type="text" placeholder="Full Name" />
-        <input type="password" placeholder="Password" />
+        <input type="email" placeholder="Email" />
+        <input type={showPass ? "text" : "password"} placeholder="Password" />
+        <span onClick={showPassword} className={styles.showPass}>
+          {showPass ? (
+            <VisibilityOffOutlinedIcon className={styles.showPassIcon} />
+          ) : (
+            <VisibilityOutlinedIcon className={styles.showPassIcon} />
+          )}
+        </span>
         <button className={styles.loginBtn}>Login</button>
       </form>
+      <div className={styles.bottom}>
+        <Link className={styles.forgotPassword} to="/forgot_password">
+          Forgot Password?
+        </Link>
+        <Link className={styles.register} to="/register">
+          Create New Account
+        </Link>
+      </div>
     </div>
   );
 };
